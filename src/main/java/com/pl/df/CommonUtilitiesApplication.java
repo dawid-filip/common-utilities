5a88840a22c2fe553a4f721cb1bd59c1a749e778
dawid <dpilif@gmail.com>
2021-07-31 21:33:34 +0200

    Add empty springboot project.

---
+++ src/main/java/com/pl/df/CommonUtilitiesApplication.java
@@ -0,0 +1,13 @@
+package com.pl.df;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonUtilitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonUtilitiesApplication.class, args);
+	}
+
+}
